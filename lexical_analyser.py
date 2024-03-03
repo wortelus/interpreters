f503475ddd3bc848bec11f1042942f55fce748c9
wortelus <daniel.slavik.cz@gmail.com>
2024-03-03 22:33:44 +0100

    Done lexical analyser

--- lexical_analyser.py
+++ lexical_analyser.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+global unknown_kw
+
 TokenType = Enum("TYPE", ["NUM",
                           "OP",
                           "LPAR",
@@ -7,6 +9,7 @@ TokenType = Enum("TYPE", ["NUM",
                           "SEMICOLON",
                           "DIV",
                           "MOD",
+                          "ID",
                           ])
 
 
@@ -16,7 +19,10 @@ class Token:
         self.value = value
 
     def __str__(self):
-        return f"{self.type}:{self.value}"
+        if self.value is not None:
+            return f"{self.type}:{self.value}"
+        else:
+            return f"{self.type}"
 
     def __repr__(self):
         return self.__str__()
@@ -27,10 +33,22 @@ def tokenize(expression):
 
     number = ''
     cmd = 0
+
+    global unknown_kw
+    unknown_kw = ""
     keywords = {
         "div": 0,
         "mod": 0,
     }
+
+    def reset_keywords():
+        global unknown_kw
+        if unknown_kw != "" and unknown_kw not in keywords.keys():
+            tokens.append(Token('ID', unknown_kw))
+            unknown_kw = ""
+        for k in keywords.keys():
+            keywords[k] = 0
+
     for char in expression:
 
         # Handle comments
@@ -41,6 +59,8 @@ def tokenize(expression):
         elif char == '/':
             if cmd == 1:
                 cmd += 1
+                tokens.pop()
+                continue
             else:
                 cmd = 1
         else:
@@ -48,8 +68,12 @@ def tokenize(expression):
 
         # Handle tokens
         if char.isdigit():
+            # reset keywords
+            reset_keywords()
             number += char
-        elif char == ' ':
+        elif char == ' ' or char == '\n':
+            # reset keywords
+            reset_keywords()
             continue
         else:
             # Handle number construction
@@ -60,20 +84,22 @@ def tokenize(expression):
             if char == ';':
                 tokens.append(Token('SEMICOLON', char))
             if char == '(':
-                tokens.append(Token('LPAR', char))
+                tokens.append(Token('LPAR', None))
             elif char == ')':
-                tokens.append(Token('RPAR', char))
+                tokens.append(Token('RPAR', None))
             elif char in "+-*":
                 tokens.append(Token('OP', char))
             elif char == '/':
-                tokens.append(Token('DIV', char))
+                tokens.append(Token('DIV', None))
             # Handle multiple chars
             else:
+                unknown_kw += char
                 for keyword in keywords:
                     if keyword.startswith(char[keywords[keyword]:]):
                         keywords[keyword] += 1
                         if keywords[keyword] == len(keyword):
-                            tokens.append(Token(keyword.upper(), keyword))
+                            tokens.append(Token(keyword.upper(), None))
+                            unknown_kw = ""
                             keywords[keyword] = 0
                     else:
                         keywords[keyword] = 0
@@ -83,7 +109,10 @@ def tokenize(expression):
     return tokens
 
 
-input_str = input("Zadej výraz: \n")
-tokens = tokenize(input_str)
+expr = ""
+while input_str := input(">"):
+    expr += input_str + "\n"
+
+tokens = tokenize(expr)
 for token in tokens:
     print(token)
